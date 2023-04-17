af4f97eb0c1bf5d2c175eeb8ad54fa9f51ffe600
Ivo Nunes <inunes1904@gmail.com>
2023-04-17 12:42:45 +0100

    Nodes removeLast

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,25 +1,57 @@
 public class LinkedList {
-
   private Node head;
   private Node tail;
   private int length;
 
-  public LinkedList(int value) {
-    Node newNode = new Node(value);
-    head = newNode;
-    length = 1;
-  }
-  public void append(int value) {
-    tail = head;
-    Node newNode = new Node(value);
-    head = newNode;
-    length++;
-  }
-  public void prepend(int value) {
+  public class Node{
+
+    int value;
+
+    Node next;
+
+    public Node(int value) {
+      this.value = value;
+    }
+
   }
 
-  public boolean insert(int index, int value) {
-  return true;
+  public LinkedList(int value) {
+    Node newNode = new Node(value);
+    this.head = newNode;
+    this.tail = newNode;
+    this.length = 1;
+  }
+
+  public void append(int value){
+    Node newNode =  new Node(value);
+    if (this.length > 0){
+      tail.next = newNode;
+      this.length++;
+      tail = newNode;
+    }else{
+      this.head = newNode;
+      this.tail = newNode;
+      this.length = 1;
+    }
+  }
+
+
+  public Node removeLast(){
+    if (length == 0) return null;
+    Node startNode = head;
+    Node tmpNode = startNode;
+    while ( tmpNode.next != null ){
+        startNode = tmpNode;
+        tmpNode = tmpNode.next;
+    }
+    startNode.next = null;
+    tail = startNode;
+    length--;
+    if (length == 0){
+      head = null;
+      tail = null;
+    }
+    return tmpNode;
   }
 
 }
