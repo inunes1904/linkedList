45ac14fbc1e56671f799d33ad83ad0bfeefae91e
Ivo Nunes <inunes1904@gmail.com>
2023-04-17 13:24:38 +0100

    exercise 3 removeLast

---
+++ src/ExerciseThree/LinkedList.java
@@ -0,0 +1,85 @@
+package ExerciseThree;
+
+public class LinkedList {
+
+  private Node head;
+  private Node tail;
+  private int length;
+
+  class Node {
+    int value;
+    Node next;
+
+    Node(int value) {
+      this.value = value;
+    }
+  }
+
+  public LinkedList(int value) {
+    Node newNode = new Node(value);
+    head = newNode;
+    tail = newNode;
+    length = 1;
+  }
+
+  public void printList() {
+    Node temp = head;
+    while (temp != null) {
+      System.out.println(temp.value);
+      temp = temp.next;
+    }
+  }
+
+  public void getHead() {
+    if (head == null) {
+      System.out.println("Head: null");
+    } else {
+      System.out.println("Head: " + head.value);
+    }
+  }
+
+  public void getTail() {
+    if (head == null) {
+      System.out.println("Tail: null");
+    } else {
+      System.out.println("Tail: " + tail.value);
+    }
+  }
+
+  public void getLength() {
+    System.out.println("Length: " + length);
+  }
+
+  public void append(int value) {
+    Node newNode = new Node(value);
+    if (length == 0) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      tail.next = newNode;
+      tail = newNode;
+    }
+    length++;
+  }
+
+  // WRITE REMOVELAST METHOD HERE //
+  public Node removeLast(){
+
+    if (length == 0 ) return null;
+    Node tmpNode = head;
+    Node startNode = tmpNode;
+
+    while ( tmpNode.next != null){
+      startNode = tmpNode;
+      tmpNode = tmpNode.next;
+    }
+    startNode.next = null;
+    length--;
+    tail = startNode;
+    if (length == 0){
+      tail = null;
+      head = null;
+    }
+    return tmpNode;
+  }
+}
