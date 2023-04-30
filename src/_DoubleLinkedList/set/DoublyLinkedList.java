16240684aeb26640709f90469ee07de6d08604e7
Ivo Nunes <inunes1904@gmail.com>
2023-04-30 18:14:11 +0100

    insert Double Linked List

---
+++ src/_DoubleLinkedList/set/DoublyLinkedList.java
@@ -0,0 +1,134 @@
+package _DoubleLinkedList.set;
+
+public class DoublyLinkedList {
+
+  private Node head;
+  private Node tail;
+  private int length;
+
+  class Node {
+    int value;
+    Node next;
+    Node prev;
+
+    Node(int value) {
+      this.value = value;
+    }
+  }
+
+  public DoublyLinkedList(int value) {
+    Node newNode = new Node(value);
+    head = newNode;
+    tail = newNode;
+    length = 1;
+  }
+
+  public void printList() {
+    Node temp = head;
+    while (temp != null) {
+      System.out.println(temp.value);
+      temp = temp.next;
+    }
+  }
+
+  public void getHead() {
+    if (head == null) {
+      System.out.println("Head: null");
+    } else {
+      System.out.println("Head: " + head.value);
+    }
+  }
+
+  public void getTail() {
+    if (head == null) {
+      System.out.println("Tail: null");
+    } else {
+      System.out.println("Tail: " + tail.value);
+    }
+  }
+
+  public void getLength() {
+    System.out.println("Length: " + length);
+  }
+
+  public void append (int value) {
+    Node newNode = new Node(value);
+    if(length == 0) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      tail.next = newNode;
+      newNode.prev = tail;
+      tail = newNode;
+    }
+    length++;
+  }
+
+  public Node removeLast() {
+    if(length == 0) return null;
+    Node temp = tail;
+    if (length == 1) {
+      head = null;
+      tail = null;
+    } else {
+      tail = tail.prev;
+      tail.next = null;
+      temp.prev = null;
+    }
+    length--;
+    return temp;
+  }
+
+  public void prepend(int value) {
+    Node newNode = new Node(value);
+    if(length == 0) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      newNode.next = head;
+      head.prev = newNode;
+      head = newNode;
+    }
+    length++;
+  }
+
+  public Node removeFirst() {
+    if(length == 0) return null;
+    Node temp = head;
+    if(length == 1) {
+      head = null;
+      tail = null;
+    } else {
+      head = head.next;
+      head.prev = null;
+      temp.next = null;
+    }
+    length--;
+    return temp;
+  }
+
+  public Node get(int index) {
+    if (index < 0 || index >= length) return null;
+    Node temp = head;
+    if (index < length/2) {
+      for (int i = 0; i < index; i++) {
+        temp = temp.next;
+      }
+    } else {
+      temp = tail;
+      for (int i = length - 1; i > index; i--) {
+        temp = temp.prev;
+      }
+    }
+    return temp;
+  }
+
+  /// WRITE SET METHOD HERE ///
+public boolean set(int index, int value){
+    if (index < 0 || index >= length ) return false;
+    Node tmp = get(index);
+    tmp.value = value;
+    return true;
+}
+
+}
